8112a4e0b7a58a0c9131da7dad14001c01b4ddf9
amandine <amande.bourget@gmail.com>
2016-01-02 04:50:39 +0100

    modification notification push en retard

--- src/main/java/controller/NotificationRESTController.java
+++ src/main/java/controller/NotificationRESTController.java
@@ -1,11 +1,11 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Date;
 import java.util.List;
+
+import model.ListNotification;
+import model.Notification;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,14 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dao.AreaJDBCTemplate;
 import dao.NotificationJDBCTemplate;
 import dao.UserJDBCTemplate;
-import model.Area;
-import model.ListArea;
-import model.ListNotification;
-import model.Notification;
-import model.User;
 
 @RestController
 public class NotificationRESTController
@@ -47,21 +41,21 @@ public class NotificationRESTController
 		if(id_user!=null){ // si l'id_user existe
 			NotificationJDBCTemplate notificationJDBCTemplate= (NotificationJDBCTemplate)context.getBean("notificationJDBCTemplate");
 			System.out.println("state : "+state);
-			//on récupère la dernière notification de l'utilisateur
+			//on rï¿½cupï¿½re la derniï¿½re notification de l'utilisateur
 			Notification lastnotifByUser =notificationJDBCTemplate.getLastNotification(id_user);			
 			
 			if(state.equals("processing")){ // si la sortie de zone est en cours
 				
-				if(lastnotifByUser.getFlag_processing()!=null && lastnotifByUser.getFlag_processing()==0){ //si il y a déja une notif en cours
+				if(lastnotifByUser.getFlag_processing()!=null && lastnotifByUser.getFlag_processing()==0){ //si il y a dï¿½ja une notif en cours
 					//on update les positions, la date	et le flag_processing =0
 					System.out.println("update de la notif");
 					notificationJDBCTemplate.updateNotif(lastnotifByUser.getId_notification(), lat, lon,new Date(), new Integer(0));
-				}else{ // on créé la notification
+				}else{ // on crï¿½ï¿½ la notification
 					System.out.println("creation de la notif");
 					notificationJDBCTemplate.createNotification(id_user, lat, lon, new Date(), new Integer(0));
 				}
 
-			}else{ // si la sortie de zone est terminée
+			}else{ // si la sortie de zone est terminï¿½e
 				//on update les positions, la date et le flag_processing =1
 				notificationJDBCTemplate.updateNotif(lastnotifByUser.getId_notification(), lat, lon,new Date(), new Integer(1));
 			}
@@ -92,7 +86,7 @@ public class NotificationRESTController
 	    	 
 	    	 if(id_user!=null){ 
 	    	
-	    	 // a partir de l'id_user on retrouve la liste des zones associées a cet id
+	    	 // a partir de l'id_user on retrouve la liste des zones associï¿½es a cet id
 	    	 NotificationJDBCTemplate notificationJDBCTemplate = 
 	       	      (NotificationJDBCTemplate)context.getBean("notificationJDBCTemplate");
 	    	 listNotification=notificationJDBCTemplate.listNotificationByIdUser(id_user);
