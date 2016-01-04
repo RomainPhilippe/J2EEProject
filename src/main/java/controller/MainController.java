4401fa2586b5b455b3bb7bf080cffc27beafc82d
PHILIPPE Romain <romain.philippe@isep.fr>
2016-01-04 14:02:24 +0100

    Update MainController.java

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import dao.NotificationJDBCTemplate;
 import dao.UserDao;
@@ -30,56 +31,93 @@ import model.User;
 
 //controler principal
 @Controller
+@SessionAttributes("token")
+
 public class MainController {
-	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-	List<Notification> listNotification = null;
-
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public ModelAndView home() {
-		return new ModelAndView("home", "command", new Authentification());
-	}
-
-	@RequestMapping(value = "/authentification", method = RequestMethod.POST)
-	public ModelAndView addStudent(@ModelAttribute("SpringWeb") Authentification student, ModelMap model) {
+	
+		List<Notification> listNotification=null;
 		
-		try {
-			
-			UserJDBCTemplate userJDBCTemplate = (UserJDBCTemplate) context.getBean("userJDBCTemplate");
-			String isValidUser = userJDBCTemplate.identificationParent(student.getEmail(), student.getPassword());
-			if (isValidUser != null) {
-				System.out.println("User Login Successful");
+		@RequestMapping(value = "/home", method = RequestMethod.GET)
+		public ModelAndView home() {
+			return new ModelAndView("home", "command", new Authentification());
+		}
+		
+		@RequestMapping(value="/home",method=RequestMethod.POST)
+		public ModelAndView executeLogin()
+		{
+				System.out.println("home");
 				
-				List<Notification> list = getList();
-				// return back to profil.jsp
-				ModelAndView model1 = new ModelAndView("profil");
-				model1.addObject("listNotif", list);
-				return model1;
-				
-			} else {
-				System.out.println("User Login failed");
+				return new ModelAndView("profil");
+		}
+		
+		@RequestMapping(value = "/student", method = RequestMethod.GET)
+		   public ModelAndView student() {
+		      return new ModelAndView("student", "command", new Authentification());
+		      //return "student";
+		   }
+		   
+		   
+		   @RequestMapping(value = "/authentification", method = RequestMethod.POST)
+		   public ModelAndView addStudent(@ModelAttribute("SpringWeb")Authentification student, 
+		   ModelMap model) {
+			   System.out.println("name : "+student.getEmail());
+			   System.out.println("age : "+student.getPassword());
+
+			   
+			   
+				try
+				{
+					ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+					UserJDBCTemplate userJDBCTemplate = (UserJDBCTemplate) context.getBean("userJDBCTemplate");
+						User user = userJDBCTemplate.identificationParent2(student.getEmail(), student.getPassword());
+						if(user.getToken() != null)
+						{
+							List<Notification> list = getList();
+								ModelAndView modelAndView = new ModelAndView("profil");
+								System.out.println("User Login Successful");
+								modelAndView.addObject("token", user.getToken());
+								modelAndView.addObject("id_user", user.getId_user());
+								modelAndView.addObject("listNotif", list);
+								return modelAndView;
+						}else{
+							    return new ModelAndView("home", "command", new Authentification());
+						}
+
+				}
+				catch(Exception e)
+				{
+						e.printStackTrace();
+				}
 				return new ModelAndView("home", "command", new Authentification());
+		      
+		   }
+
+		
+		@RequestMapping(value = "/profil", method = RequestMethod.GET)
+			public ModelAndView profil() 
+			{
+				List<Notification> list = getList();
+				//return back to profil.jsp
+				ModelAndView model = new ModelAndView("profil");
+				model.addObject("listNotif", list);
+			
+				return model;
 			}
 
-		} catch (Exception e) {
-			e.printStackTrace();
+		private List<Notification> getList() {
+
+			List<Notification> list = new ArrayList<Notification>();
+			
+			//creation area en local
+			Integer id_user=1;
+	    	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+			NotificationJDBCTemplate notificationJDBCTemplate = (NotificationJDBCTemplate)context.getBean("notificationJDBCTemplate");
+	    	
+			listNotification= notificationJDBCTemplate.listNotificationByIdUser(id_user);
+
+			return listNotification;
+
 		}
-		return new ModelAndView("peo", "command", new Authentification());
-
+		
+		
 	}
-
-	private List<Notification> getList() {
-
-		List<Notification> list = new ArrayList<Notification>();
-
-		// creation area en local
-		Integer id_user = 1;
-		NotificationJDBCTemplate notificationJDBCTemplate = (NotificationJDBCTemplate) context
-				.getBean("notificationJDBCTemplate");
-
-		listNotification = notificationJDBCTemplate.listNotificationByIdUser(id_user);
-
-		return listNotification;
-
-	}
-
-}
